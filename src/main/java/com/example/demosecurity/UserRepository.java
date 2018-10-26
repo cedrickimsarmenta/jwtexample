d168f02437c692aae89dfa924c2b415536d6d964
cedric.sarmenta <Cedric.Kim.Sarmenta@asia.ing.com>
2018-10-26 11:44:05 +0800

    Initial Spring App with user and role domains.

---
+++ src/main/java/com/example/demosecurity/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demosecurity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1")
+    Optional<User> findByEmail(String email);
+}
